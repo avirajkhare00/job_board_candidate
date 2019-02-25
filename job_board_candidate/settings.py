a89d4cd0facab77a5ce4813069dfd71745aa2858
Aviraj Khare <avirajkhare00@gmail.com>
2019-02-25 18:35:53 +0530

    prod settings updated in new branch

--- job_board_candidate/settings.py
+++ job_board_candidate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rabifbez6w-6d(o+6%fq^m=0rsw$jt-i7#ey)%4mk!pbk1u88o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jobboard.hellomeets.com']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'job_board_candidate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'job_board',
+        'USER': 'admin',
+        'PASSWORD': 'foryoureyesonly',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -121,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
